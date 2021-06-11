bca52d1361e04c677e2a48142722a93ab6f6c01f
Maurice Jones <jones.maurice39@yahoo.com>
2021-06-10 20:33:22 -0700

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,9 +31,7 @@ function Header() {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
-            <ProfilePic>
-
-            </ProfilePic>
+            <UserImg src="/images/Weightlifting-Gym-Zoom-Background.jpg" />
         </Nav>
     )
 }
@@ -53,11 +51,15 @@ const Logo = styled.img`
 `
 const NavMenu = styled.div`
     display: flex;
-    
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
+
     a{
         display: flex;
         align-items: center;
         padding: 0 12px;
+        cursor: pointer;
 
         img {
             height: 20px;
@@ -66,9 +68,35 @@ const NavMenu = styled.div`
         span{
             font-size: 13px;
             letter-spacing: 1.42px;
+            position: relative;
+
+            &:after {
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity: 0;
+                transform-orgin: left center;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
         }
+
+        &:hover {
+            span:after {
+                transform: scaleX(1);
+                opacity: 1;
+            }
+        }
+
     }
 `
-const ProfilePic = styled.div`
-
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
 `
